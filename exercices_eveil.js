e334961783bccbc39b2f23ce38a879d628123744
jeremylejoly <jeremylejoly@gmail.com>
2026-06-28 21:37:12 +0200

    ajout logo

--- exercices_eveil.js
+++ exercices_eveil.js
@@ -362,6 +362,7 @@ window.SCI_RESP_QCM_DATA = [
 
 window.EMOJI_SVG_MAP = {
     '🔠': `<img src="assets/logos/jeu_mots_croises.png" class="w-full h-full object-contain" alt="Mots croisés" />`,
+    '🔎': `<img src="assets/logos/jeu_mots_caches.png" class="w-full h-full object-contain" alt="Mots cachés" />`,
     // Emojis de Français, Conjugaison & Lecture
     '📚': `<img src="assets/logos/lecture_narrative.png" class="w-full h-full object-contain" alt="Lecture" />`,
     '📖': `<img src="assets/logos/lecture_informative.png" class="w-full h-full object-contain" alt="Lecture" />`,
