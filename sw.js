baf94186aaec0fabed33d7ccae96aba897a80d50
jeremylejoly <jeremylejoly@gmail.com>
2026-09-19 13:58:05 +0200

    Ajout du 9ème jeu : Flux Connecté avec design LCML et logo personnalisé

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'classe-mr-lejoly-cache-v330';
+const CACHE_NAME = 'classe-mr-lejoly-cache-v331';
 const CORE_ASSETS = [
   './',
   'index.html',
@@ -10,6 +10,8 @@ const CORE_ASSETS = [
   'assets/logos/jeu_mots_caches.png',
   'sudoku.html',
   'assets/logos/jeu_sudoku.png',
+  'flux_connecte.html',
+  'assets/logos/jeu_flux.png',
   'assets/logos/sub_durees_conversions.png',
   'assets/logos/sub_durees_duree.png',
   'assets/logos/sub_durees_heure.png',
