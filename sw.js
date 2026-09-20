67b649d37309f844e114342f9e0b3369a18b9280
jeremylejoly <jeremylejoly@gmail.com>
2026-09-20 12:03:59 +0200

    feat: ajout et intégration du jeu Les Pentominos avec nouveau design et logo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'classe-mr-lejoly-cache-v332';
+const CACHE_NAME = 'classe-mr-lejoly-cache-v333';
 const CORE_ASSETS = [
   './',
   'index.html',
@@ -15,6 +15,9 @@ const CORE_ASSETS = [
   'jeu_code_secret.html',
   'fiches/jeu_code_secret.html',
   'assets/logos/jeu_code_secret.png',
+  'pentomino.html',
+  'fiches/pentomino.html',
+  'assets/logos/jeu_pentomino.png',
   'assets/logos/sub_durees_conversions.png',
   'assets/logos/sub_durees_duree.png',
   'assets/logos/sub_durees_heure.png',
