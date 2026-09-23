34f59c84670cf3b6ff91d5967225ec61064ee38a
jeremylejoly <jeremylejoly@gmail.com>
2026-09-23 19:42:22 +0200

    Résultats sans doublons ni pertes + images compressées (-70 %) + SW v344

--- sw.js
+++ sw.js
@@ -13,8 +13,8 @@
  *  ➜ Si vous remplacez une image en gardant le même nom et voulez forcer la mise à jour
  *    immédiate sur les tablettes : incrémenter MEDIA_VERSION.
  */
-const VERSION = 'v343';
-const MEDIA_VERSION = 'm1';
+const VERSION = 'v344';
+const MEDIA_VERSION = 'm2';
 
 const CORE_CACHE  = 'lcml-core-'  + VERSION;
 const PAGES_CACHE = 'lcml-pages-' + VERSION;
