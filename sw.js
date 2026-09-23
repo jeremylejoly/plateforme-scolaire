8c058eeabd6b6339c26afaef22f43bbfdc63a91e
jeremylejoly <jeremylejoly@gmail.com>
2026-09-23 20:40:51 +0200

    corr bug changement élève

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  *  ➜ Si vous remplacez une image en gardant le même nom et voulez forcer la mise à jour
  *    immédiate sur les tablettes : incrémenter MEDIA_VERSION.
  */
-const VERSION = 'v345';
+const VERSION = 'v346';
 const MEDIA_VERSION = 'm2';
 
 const CORE_CACHE  = 'lcml-core-'  + VERSION;
