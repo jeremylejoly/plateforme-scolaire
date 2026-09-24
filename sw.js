2da9c20d51cc6de1c8f25ae0a42ae3882c392067
jeremylejoly <jeremylejoly@gmail.com>
2026-09-24 18:50:15 +0200

    feat(lecture): ajout du bouton retour en haut à gauche lors d'un questionnaire et maj sw v350

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  *  ➜ Si vous remplacez une image en gardant le même nom et voulez forcer la mise à jour
  *    immédiate sur les tablettes : incrémenter MEDIA_VERSION.
  */
-const VERSION = 'v349';
+const VERSION = 'v350';
 const MEDIA_VERSION = 'm3';
 
 const CORE_CACHE  = 'lcml-core-'  + VERSION;
